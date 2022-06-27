3a162abfb8dadff7441238a770880453a0d28e73
Sezarchik <99130273+Sezarchik@users.noreply.github.com>
2022-06-27 18:34:51 +0500

    Update and rename KeyboardSwitcher (2).py to KeyboardSwitcher.py

--- KeyboardSwitcher.py
+++ KeyboardSwitcher.py
@@ -1,20 +1,4 @@
-# -*- coding: utf-8 -*-
-
-#   Friendly Telegram (telegram userbot)
-#   Copyright (C) 2018-2020 @DneZyeK | sub to @KeyZenD
-
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Affero General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Affero General Public License for more details.
-
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# meta developer: tg://openmessage?user_id=1707984808
 
 import logging
 from .. import loader, utils
@@ -45,13 +29,13 @@ class KeyboardSwitcherMod(loader.Module):
             reply = await message.get_reply_message()
             text = reply.raw_text
             if not text:
-                await message.edit('Тут текста нету...')
+                message = await utils.answer(message, 'Тут текста нету...')
                 return
             change = str.maketrans(RuKeys + EnKeys, EnKeys + RuKeys)
             text = str.translate(text, change)
 
             if message.sender_id != reply.sender_id:
-                await message.edit(text)
+                message = await utils.answer(message, text)
             else:
                 await message.delete()
                 await reply.edit(text)
@@ -59,8 +43,8 @@ class KeyboardSwitcherMod(loader.Module):
         else:
             text = utils.get_args_raw(message)
             if not text:
-                await message.edit('Тут текста нету...')
+                message = await utils.answer(message, 'Тут текста нету...')
                 return
             change = str.maketrans(RuKeys + EnKeys, EnKeys + RuKeys)
             text = str.translate(text, change)
-            await message.edit(text)
+            message = await utils.answer(message, text)
